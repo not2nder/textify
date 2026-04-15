99c15a36dcfb0957de6aa49d18a59b365cd1679d
not2nder <brtiodark@gmail.com>
2026-04-15 00:11:13 -0300

    Primeiro Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# ✨ Textify
+
+Converta qualquer texto em fontes estilizadas de forma rápida e simples.
+![image](https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB) ![image](https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white)
+
+## 📖 Sobre o projeto
+
+O **Textify** é uma aplicação web que transforma textos comuns em diferentes estilos de fontes utilizando caracteres Unicode. Ideal para uso em redes sociais, bios, nomes personalizados e qualquer lugar onde você queira destacar seu texto. O projeto é uma versão mais moderna e atualizada do meu antigo projeto: [Fancy Words](https://gtihub.com/not2nder/fancy-words)
+
+---
+
+## 🛠️ Tecnologias utilizadas
+
+- React
+- Vite
+- Tailwind CSS
+
+## 📌 Melhorias futuras
+
+- Botão de copiar com feedback
+- Mais estilos de fontes
+- Categorias de fontes
+- Responsividade aprimorada
---
+++ src/components/TextItems.jsx
@@ -0,0 +1,38 @@
+import { Clipboard } from "lucide-react";
+
+function TextItems({ fonts, text }) {
+  function stylize(text, charset) {
+    return text
+      .split("")
+      .map((char) => charset[char] || char)
+      .join("");
+  }
+
+  const copyToClipboard = async (text) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      alert("Copied to clipboard!");
+    } catch (err) {
+      console.error("Failed to copy: ", err);
+    }
+  };
+
+  const output = text.trim() ? text.trim() : "Textify";
+
+  return (
+    <ul className="h-[280px] space-y-4 overflow-y-scroll no-scrollbar">
+      {fonts.map((font, index) => (
+        <li
+          key={index}
+          className="w-100 flex justify-between px-4 py-3 bg-slate-200 shadow rounded-xl hover:bg-slate-300 hover:shadow-lg hover:shadow-slate-200 transition-all duration-300 cursor-pointer border border-slate-200"
+          onClick={() => copyToClipboard(stylize(output, font))}
+        >
+          <p className="truncate text-slate-900">{stylize(output, font)}</p>
+          <Clipboard className="text-slate-500" />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default TextItems;
